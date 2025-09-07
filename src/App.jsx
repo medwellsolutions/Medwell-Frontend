cf4475da574384e3b8b00d89169a5858ef8b7198
kalva pranai sai reddy <kalvapranaisaireddy@gmail.com>
2025-09-06 19:18:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import Home from './components/Home';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path = '/' element = {<Body/>}>
+        <Route path = '/' element = {<Home/>}></Route>
+        <Route path = '/login' element = {<Login/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
